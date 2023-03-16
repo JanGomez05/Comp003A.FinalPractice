6f4d9f3776263f5cce7d1fb8b120100ad27a2be0
Jan Gomez <123139695+JanGomez05@users.noreply.github.com>
2023-03-15 20:01:10 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,163 +1,167 @@
-﻿namespace Comp003A.LectureActivity5
+﻿/*
+ * Author: Jan Gomez
+ * Purpose: Final Assignment
+ * Course: Comp003A L01
+ * 
+ */
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace finalproject_from_class
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("".PadRight(50, '*'));
-            Console.WriteLine("Methods & XML Comments");
-            Console.WriteLine("".PadRight(50, '*'));
-            Console.WriteLine("\n");
+            // theme would be for health patient.
+            SectionSeparator("Welcome to Hanfords Clinic!" +
+             " Here is a Registry for new Patients." +
+             " Please Answer and Label the inquiery.");
 
-            HelloWorld(); // call to HelloWorld()
-            HelloWorld("Grace Hopper"); // call to HelloWorld(string name)
-            HelloWorld("Donald Knuth"); // call to HelloWorld(string name) again with a
-            // different parameter value
-            HelloWorld("Alan Turing", DateTime.Now.Year - 1912); //call to 
-            //HelloWorld(string name, int inputAge), which is an overloaded method
-            HelloWorld("Ada Lovelace", DateTime.Now.Year - 1815); // call to
-            //HelloWorld(string name, int inputAge) again
-            HelloWorld("Charles Babbage", DateTime.Now.Year - 1791); // call to 
-            //HelloWorld(string name, int inputAge) again
+            Console.WriteLine("Enter a first name");
+            string firstName = Run(Console.ReadLine());
 
-            Console.WriteLine("\n");
-            Console.WriteLine("Enter value for inputNumber1: ");
-            decimal inputNumber1 = Convert.ToDecimal(Console.ReadLine());
-            Console.WriteLine("Enter value for inputNumber2: ");
-            decimal inputNumber2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("PLease enter yout Last Name");
+            string lastName = Run(Console.ReadLine());
 
-            //call to Sum(decimal num1, decimal num2) and storing the return value in a variable
-            decimal sum = Add(inputNumber1, inputNumber2);
-            Console.WriteLine($"Summation: {sum}"); // output the value of sum to console
+            Console.WriteLine($"\n So you are {firstName} {lastName}");
+            int year = 0;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("When were you born?");
+                    year = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(AgeCalculator(year));
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter an actual integer");
 
-            // if you don't plan on using a variable, you can simplify the method call like the lines below
-            Console.WriteLine($"Substraction: {Subtract(inputNumber1, inputNumber2)}");
-            Console.WriteLine($"Multiplication: {Multiply(inputNumber1, inputNumber2)}");
-            Console.WriteLine($"Division: {Divide(inputNumber1, inputNumber2)}");
-            Console.WriteLine($"Remainder: {Remainder(inputNumber1, inputNumber2)}");
+                }
+            } while (year >= 1900 || year <= 2010);
+           
 
-            // methods can call other methods too
-            // below is an example of a method calling itself, also known as recursion
-            Console.WriteLine("\n");
-            Inception("This is inception level ", 5);
-        } // Main() close
+            char gender;
+
+            Console.WriteLine("Pease insert your gender as M/m , F/f or O/o:");
+            gender = Convert.ToChar(Console.ReadLine());
+
+            switch (gender)
+            {
+                case 'M':
+                case 'm':
+                    Console.WriteLine($"{firstName} you are Male");
+                    break;
+                case 'F':
+                case 'f':
+                    Console.WriteLine($"{firstName} you are Female");
+                    break;
+                case 'O':
+                case 'o':
+                    Console.WriteLine($"{firstName} you are Other");
+                    break;
+
+                default:
+                    Console.WriteLine($"{firstName}, please enter out of the choices selected");
+                    break;
+            }
 
 
-        /* XML comments are prefixed with three forward slashes '///'
-         * if you type'///' on top of a class or method,
-         * visual studio will populate the structure for you 
-         * otherwise, you would have to use the following opening AND closing tags
-         * (similar to most html elements)
-         * <summary></summary> used to describe a type member
-         * <param name="nameOfTheParameter"></param> used for describing one or more 
-         * parameters in the method signature
-         * <returns></returns> used to describe the return value
-         * 
-         * there are other tags in
-         * https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/xmldoc/recommended-tags
-         */
+            List<string> response = new List<string>();
+
+            List<string> question = new List<string>();
+
+            question.Add("Are you enjoying your visit?");
+
+            question.Add("Are You currently feeling pain?");
+
+            question.Add("In the past few months have you been out of the country?");
+
+            question.Add("Have you been having fever like symptoms?");
+
+            question.Add("Have you tested for covid-19 in the past 2-4 weeks?");
+
+            question.Add("Are you physically active?");
+
+            question.Add("Do you smoke?");
+
+            question.Add("Do you drink alcohol?");
+
+            question.Add("Have you been consuming drugs?");
+
+            question.Add("Do you have any allergins?");
 
 
-        // NOTE: Method definitions are programmed inside the class { } code block, not inside Main()  {  }
-        
-        /// <summary>
-        /// Method without parameters and return value
-        /// </summary>
-        static void HelloWorld()
-        {
-            Console.WriteLine("Hello World!");
-        }
-        
-        /// <summary>
-        /// Method overloading with one parameter but no return value
-        /// Method overloading => same method name but different signature (different parameters)
-        /// </summary>
-        /// <param name="inputName">String input</param>
-        static void HelloWorld(string inputName)
-        {
-            Console.WriteLine($"Hello, {inputName}");
-        }
+            foreach (string a in question)
+            {
 
-        /// <summary>
-        /// Method overloading with multiple parameters but no return value
-        /// </summary>
-        /// <param name="inputName">String input></param>
-        /// <param name="inputAge">Integer input</param>
-        static void HelloWorld(string inputName, int inputAge)
-        {
-            Console.WriteLine($"Hello, {inputName}, age {inputAge}");
-            //you can add more code here
-        }
+                Console.WriteLine(a);
+                string answer = Console.ReadLine();
+                response.Add(answer);
 
-        ///<summary>
-        /// Method for adding two numeric variables
-        /// </summary>
-        /// <param name="num1">Decimal input</param>
-        /// <param name="num2">Decimal input</param>
-        /// <returns>Sum of two decimal inputs</returns>
-       static decimal Add(decimal num1, decimal num2)
-        {
-            //you can add more code here before the return statement
-            return num1 + num2;   
-        }
-        
-        /// <summary>
-        /// Method for subtracting two numeric variables
-        /// </summary>
-        /// <param name="num1">Decimal input</param>
-        /// <param name="num2">Decimal input</param>
-        /// <returns>Difference of two decimal inputs</returns>
-        static decimal Subtract(decimal num1, decimal num2)
-        {
-            return num1 - num2;
-        }
+            }
+
+
+        }// end of main code.
 
         /// <summary>
-        /// Method for multiplying  two numeric variables
+        /// Leaving title for the final
         /// </summary>
-        /// <param name="num1">Decimal input</param>
-        /// <param name="num2">Decimal input</param>
-        /// <returns>Product of two decimal inputs</returns>
-        static decimal Multiply(decimal num1, decimal num2)
+        /// <param name="section"></param>
+        static void SectionSeparator(string section)
         {
-            return num1 * num2;
+            Console.WriteLine("".PadRight(130, '*') +
+                $"\n\t\t{section} \n"
+                + "".PadRight(130, '*'));
         }
-
         /// <summary>
-        /// Method for dividing two numeric varibles
+        /// removing special characters from first name
         /// </summary>
-        /// <param name="num1">Decimal input</param>
-        /// <param name="num2">Decimal input</param>
-        /// <returns>Quotient of two decimal inputs</returns>
-        static decimal Divide(decimal num1, decimal num2)
+        /// <param name="Name"></param>
+        static string Run(string name)
         {
-            return num1 / num2;
+           
+            // Make own character set
+            Regex regex = new Regex("[@_!#$%^&*()<>?/|}{~:]");
+            // Pass the string in regex.IsMatch
+            // method
+            if (!regex.IsMatch(name))
+            {
+                Console.WriteLine("That's a good name");
+                return name;
+            }
+            else
+            {
+                Console.WriteLine("That is not a name");
+                Console.WriteLine("Please try again. Enter a valid name");
+                name = Run(Console.ReadLine());
+                return name;
+            }
         }
-
+ 
         /// <summary>
-        /// Method for getting the remainder after dividing two numeric variables
+        /// grabbing patients Birth Year
         /// </summary>
-        /// <param name="num1">Decimal input</param>
-        /// <param name="num2">Decimal input</param>
-        /// <returns>Remainder after dividing two decimal inputs</returns>
-        static decimal Remainder(decimal num1, decimal num2)
+        /// <param name="year"></param>
+        /// <returns></returns>
+        static int AgeCalculator(int year)
         {
-            return num1 % num2;
+            if (year != 0)
+            {
+                Console.WriteLine("Please choose a realistic year.");
+                year = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(AgeCalculator(year));
+            }
+         
+           // Console.WriteLine($"Your age is: {AgeCalculator(year)}");
+            return DateTime.Now.Year - year;
         }
-
-        /// <summary>
-        /// Recursive Method
-        /// </summary>
-        /// <param name="message">String input to print to console</param>
-        /// <param name="counter">Integer input used as a looping counter</param>
-        static void Inception(string message, int counter)
-        {
-            Console.WriteLine($"{message} {counter}");
-            counter--;
-            if (counter >= 0) Inception(message, counter--);
-        }
-
 
     }
-
 }
+   
